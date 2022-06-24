8085857c933851a5d5b780a2dec7cb06ca20ac7a
SooyeonLee000 <leesooyeon@gmail.com>
2022-06-24 14:44:19 -0400

    about 수정

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from blog.views import BlogView,BlogDetailView
+from blog.views import BlogView,BlogDetailView,Home
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', Home.as_view(), name='home'),
     path('blog/',BlogView.as_view(), name = "blog"),
     path('blog-detail/<int:pk>',BlogDetailView.as_view(), name='blog-detail'),
 
--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post
 
-def home(request):
-    return render(request, 'index.html',{})
+class  Home(ListView):
+    model = Post
+    template_name = 'index.html'
 
 class BlogView(ListView):
     model = Post
